4d3d6b522e3ca7de725c71f461561d8650c6ee6e
Bhagyashree1234a <1nc23cs026@ncetmail.com>
2025-11-03 15:05:19 +0530

    Crossbridge
    You are given that a mango weighs X kilograms and a truck weighs Y kilograms. You want to cross a bridge that can withstand a weight of Z kilograms.
    Find the maximum number of mangoes you can load in the truck so that you can cross the bridge safely.

---
+++ Crossbridge.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+ int main()
+ {
+ // Constrained code starts here
+ int x,y,z,r;
+ scanf("%d%d%d",&x,&y,&z);
+ if(x+y<=z)
+ {
+ r=(z-y)/x;
+ }
+ 
+ // Constrained code ends here
+ // Write your logic here
+ // Constrained code starts here
+ printf("%d",r);
+ 
+ }
